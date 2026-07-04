0630a4566ec1f9d88703451788c5468b948060fc
acklicuno <elijahwhubbard@gmail.com>
2026-07-04 00:30:34 -0400

    Refactored the entire main.py structure, added a compare function to so that I could loop through the iataCodes in the google sheet, and compare their prices with the prices from the flight data i pulled from the serp api earlier. All that is left is tweaking the sheet if i find a lower price and messaging myself as well.

--- flight_data.py
+++ flight_data.py
@@ -4,10 +4,10 @@ class FlightData:
         self.cheapest = None
 
     @staticmethod
-    def find_cheapest_flight(data, return_date):
+    def find_cheapest_flight(data):
         cheapest = min(data, key=lambda x: x["price"])
-        first_leg = cheapest["flights"][0]
-        departure_airport = first_leg["departure_airport"]["name"]
-        arrival_airport = first_leg["arrival_airport"]["name"]
-        departure_time = first_leg["departure_airport"]["time"]
-        return cheapest, departure_airport, arrival_airport, departure_time
+        # first_leg = cheapest["flights"][0]
+        # departure_airport = first_leg["departure_airport"]["name"]
+        # arrival_airport = first_leg["arrival_airport"]["name"]
+        # departure_time = first_leg["departure_airport"]["time"]
+        return cheapest
--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import requests
 import os
 import requests_cache
 from dotenv import load_dotenv
-
-
 from data_manager import DataManager
 from flight_search import FlightSearch
 from datetime import datetime, timedelta
@@ -18,20 +16,32 @@ TWILIO_SID=os.environ.get('TWILIO_SID')
 TWILIO_TOKEN=os.environ.get('TWILIO_TOKEN')
 TWILIO_NUMBER=os.environ.get('TWILIO_NUMBER')
 TWILIO_TO_NUMBER=os.environ.get('TWILIO_TO_NUMBER')
-
+# Build flight search object and search
+SEARCH = FlightSearch()
 # Build object and get data
 DATA = DataManager()
 sheet_data = DATA.get_data()
-
+DATA = FlightData()
 # Dates
 from_time= datetime.today() + timedelta(days=1)
 to_time = from_time + timedelta(days=6)
 six_months=datetime.today()+relativedelta(months=+6)
+from_place = 'MDT'
 
-# Build flight search object and search
-SEARCH = FlightSearch()
-from_place = 'LHR'
-to_place = 'CDG',
-print(SEARCH.flight_search(from_place, to_place, from_time=from_time, to_time=to_time))
+# ----- COMPARE-------
+# Iterate through each row(dict) in the sheet, and pull the IATA Code ----> into to_place
+# call flight_search on every loop for each IATA Code
+# Compare price in sheet dict, with price from discounted flight data\
+def compare(sheet):
+    for row in sheet:
+        to_place = row["iataCode"]
+        flight_data = SEARCH.flight_search(from_place, to_place, from_time=from_time, to_time=to_time)
+        # combined = flight_data["best_flights"] + flight_data["other_flights"]
+        discounted_flight_data = DATA.find_cheapest_flight(new_data)
+        if discounted_flight_data["price"] < row["price"]:
+            # Alter data in sheet
+            # Do full twilio stuff
+    return
+
 # Parse Data from flightsearch, and pass tow flightdata to filter and return cheapest flight
-print()
+new_data = compare(sheet_data)
